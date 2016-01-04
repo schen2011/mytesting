695ad7d21452ebded780391ac47218941ea33a80
schen <schenjobs@gmail.com>
2016-01-04 17:38:56 +0800

    ddd

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,7 +18,8 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
 
-        string powerurlconst = "https://xenrt.citrite.net:443/xenrt/api/v2/machine/{machinename}/power";
+        string powerurlconst = "https://xenrt.citrite.net:443/xenrt/api/v2/machine/{machinename}/power"; 
+        string borrowmachineconst = "https://xenrt.citrite.net:443/xenrt/api/v2/machine/{machinename}/lease";
 
         public async Task<ActionResult> Index()
         {
@@ -162,6 +163,68 @@ namespace WebApplication1.Controllers
                     string json = new JavaScriptSerializer().Serialize(new
                     {
                         operation = "on"
+                    });
+
+                    streamWriter.Write(json);
+                }
+
+                using (var response = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+
+                }
+            }
+        }
+
+
+        public void BorrowMachine(string machineid, string reason, string duration)
+        {
+            if (!String.IsNullOrEmpty(machineid))
+            {
+                StringBuilder st = new StringBuilder(borrowmachineconst);
+                st.Replace("{machinename}", machineid);
+                borrowmachineconst = st.ToString();
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(borrowmachineconst);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Headers.Add("X-Api-Key", "mx0hbgN97TSqTO/OxaAVuHtH+WpMoGbs5j71+g");
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Timeout = 20000;
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = new JavaScriptSerializer().Serialize(new
+                    {
+                        duration = Convert.ToInt32(duration),
+                        reason = reason
+                    });
+
+                    streamWriter.Write(json);
+                }
+
+                using (var response = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+
+                }
+            }
+        }
+
+        public void ReturnMachine(string machineid)
+        {
+            if (!String.IsNullOrEmpty(machineid))
+            {
+                StringBuilder st = new StringBuilder(borrowmachineconst);
+                st.Replace("{machinename}", machineid);
+                borrowmachineconst = st.ToString();
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(borrowmachineconst);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Headers.Add("X-Api-Key", "mx0hbgN97TSqTO/OxaAVuHtH+WpMoGbs5j71+g");
+                httpWebRequest.Method = "DELETE";
+                httpWebRequest.Timeout = 20000;
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = new JavaScriptSerializer().Serialize(new
+                    {
+                        force = true
                     });
 
                     streamWriter.Write(json);
--- WebApplication1/Scripts/custom/machinedetail.js
+++ WebApplication1/Scripts/custom/machinedetail.js
@@ -11,16 +11,18 @@ var getjobhistoryurl = "../../Home/GetJobHistroies";
 var poweroncontrollerurl = "../../Home/PowerOn";
 var poweroffcontrollerurl = "../../Home/PowerOff";
 var powercyclecontrollerurl = "../../Home/PowerCycle";
+var borrowmachineurl = "../../Home/BorrowMachine";
+var returnmachineurl = "../../Home/ReturnMachine";
 
 var mpp = angular.module('machinedetail', []);
 mpp.controller('MachineDetailController', ['$scope', '$http', function ($scope, $http) {
 
-    
+    var machinenametest = $('#machinenametest').html()
 
     $http({
         method: 'GET',
         url: machineapiurl,
-        params: { "id": "NKGXENRT-1" },
+        params: { "id": machinenametest },
     }).success(function (data, status) {
         console.log("call success");
         $scope.machinename = _.first(_.pluck(data, "name"));
@@ -47,7 +49,7 @@ mpp.controller('MachineDetailController', ['$scope', '$http', function ($scope, 
     $http({
         method: 'GET',
         url: getpowerstatusurl,
-        params: { "id": "NKGXENRT-1" },
+        params: { "id": machinenametest },
     }).success(function (data, status) {
         $scope.powerstatus = data.status;
     }).error(function (data, status) {
@@ -58,7 +60,7 @@ mpp.controller('MachineDetailController', ['$scope', '$http', function ($scope, 
     $http({
         method: 'GET',
         url: getjobhistoryurl,
-        params: { "id": "NKGXENRT-1" },
+        params: { "id": machinenametest },
     }).success(function (data, status) {
         console.log("get job history success");
         $scope.lastjob = _.last(_.pluck(data, "id"));
@@ -72,14 +74,23 @@ mpp.controller('MachineDetailController', ['$scope', '$http', function ($scope, 
     });
 
     $scope.returnmachine = function () {
-        alert("Return Machine");
+        $.post(returnmachineurl, { "machineid": $scope.machinename }, function (data, status) {
+            if ("success" == status) {
+                window.location.reload(true);
+            }
+        });
     };
 
     $scope.duration = '';
     $scope.reason = '';
-
     $scope.borrowmachine = function () {
-
+        $scope.reason = this.reason;
+        $scope.duration = this.duration;
+        $.post(borrowmachineurl, { "machineid": $scope.machinename, "reason": $scope.reason, "duration": $scope.duration }, function (data, status) {
+            if ("success" == status) {
+                window.location.reload(true);
+            }
+        });
     };
 
 
@@ -112,12 +123,10 @@ mpp.controller('MachineDetailController', ['$scope', '$http', function ($scope, 
         });
     };
 
+
+
 }])
-.directive('myjobhistory', function () {
-    return {
-        template: 'Job : {{ job.id }} <br /> Description {{ job.description }}'
-    };
-})
+
 .directive('serialport', function () {
     return {
         template: 'Unix: <text class="serialtext">ssh -t cons@ {{ ip }} {{ machinename }} (password console)</text><br />Windows: <text class="serialtext">echo {{ machinename }} > %TEMP%\'xenrt-puttycmd & putty.exe -t cons@{{ ip }} -pw console -m %TEMP%\'xenrt-puttycmd (requires PuTTY on %PATH%)</text>'
